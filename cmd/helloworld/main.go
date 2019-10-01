e177ef6ad777edbf646be68a9f83442087fa6d26
Ishan Khare <ishankhare07@gmail.com>
2019-10-01 16:43:37 +0530

    initial commit with go server and python client
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

---
+++ cmd/helloworld/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	pb "grpc-test/pkg/services/helloworld"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+var port = ":8888"
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("New request from %s", req.GetName())
+	return &pb.HelloReply{Message: "Hello " + req.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("Server listening on port %s", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf(err.Error())
+	}
+}
